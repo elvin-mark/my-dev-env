1736a5286de5bc1256b84da58433ce2d04253f83
elvin-mark <munoz.vega.em@gmail.com>
2024-06-19 00:17:51 +0900

    updating init.sh

--- my-ubuntu/init.sh
+++ my-ubuntu/init.sh
@@ -1,6 +1,13 @@
 apt update && apt install -y build-essential curl wget git zsh \
 vim \
-python3 python3-pip 
+python3 python3-pip golang
+
+# Installing rust
+curl https://sh.rustup.rs -sSf | sh
+
 
 # Installing needed python packages
 pip install -r requirements.txt --break-system-packages
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
